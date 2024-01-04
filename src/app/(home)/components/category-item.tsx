aef04be1d4a7ce782ce0d8b0fe753e2eeb2c9e03
len-on <lenon.assalin@gmail.com>
2024-01-04 19:23:57 -0300

    feat: add page to show category products

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,27 +1,19 @@
 import React from "react";
 import { Badge } from "@/components/ui/badge";
 import { Category } from "@prisma/client"
-import { HeadphonesIcon, Keyboard, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
+import { CATEGORY_ICON } from "@/app/constants/category-icon";
 interface CategoryItemProps {
   category: Category
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryItem = {
-    keyboards: <Keyboard size={16}/>,
-    monitors: <MonitorIcon size={16}/>,
-    headphones: <HeadphonesIcon size={16}/>,
-    mousepads: <SquareIcon size={16}/>,
-    speakers: <SpeakerIcon size={16}/>,
-    mouses: <MouseIcon size={16}/>
-  }
-
+  
   return (
     <Badge 
       variant={"outline"} 
       className="flex items-center justify-center gap-2 rounded-lg py-3"
     >
-      {categoryItem[category.slug as keyof typeof categoryItem]}
+      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
       <span className="text-xs font-bold">{category.name}</span>
     </Badge>
   );
--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CatalogProps {
   category: Category;
@@ -7,23 +8,25 @@ interface CatalogProps {
 
 const CatalogItem = ({ category }: CatalogProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{ objectFit: "contain" }}
-        />
-      </div>
+    <Link href={`/catalog/${category.slug}`}>
+      <div className="flex flex-col">
+        <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{ objectFit: "contain" }}
+          />
+        </div>
 
-      <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
-        <p className="text-sm font-semibold text-center">{category.name}</p>
+        <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
+          <p className="text-center text-sm font-semibold">{category.name}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
