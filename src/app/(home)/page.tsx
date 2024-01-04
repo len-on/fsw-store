f3d116bcdb0ffada20687306a93fba11bcafd3b7
len-on <lenon.assalin@gmail.com>
2024-01-04 17:17:58 -0300

    feat: add third promo banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -18,23 +18,22 @@ export default async function Home() {
     where: {
       category: {
         slug: "keyboards",
-      }  
-    }
-    
-  })
+      },
+    },
+  });
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês"
       />
 
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -44,9 +43,16 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses"
       />
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 55% de desconto em mouses"
+        />
       </div>
     </div>
   );
