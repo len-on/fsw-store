d0c5ea9450ff8616a7b0d4a5f82415ff217691ab
len-on <lenon.assalin@gmail.com>
2024-01-04 15:18:34 -0300

    chore: add remotepattern to next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fsw-store.s3.sa-east-1.amazonaws.com',
+        pathname: '**',
+      }
+    ],
+  }
+}
 
 module.exports = nextConfig
