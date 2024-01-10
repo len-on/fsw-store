c8e1c1adfb64f0bd407369e36e46b9bd9e28a968
len-on <lenon.assalin@gmail.com>
2024-01-10 14:47:06 -0300

    fix: ensure cart total and subtotal are correctly calculated

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -41,14 +41,16 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
   // Total sem descontos
   const subtotal = useMemo(() => {
     return products.reduce((acc, product) => {
-      return acc + Number(product.basePrice);
+      
+      return acc + Number(product.basePrice) * product.quantity;
     }, 0);
   }, [products])
 
   //Total com descontos
   const total = useMemo(() => {
     return products.reduce((acc, product) => {
-      return acc + Number(product.totalPrice);
+      
+      return acc + Number(product.totalPrice) * product.quantity;
     }, 0);
   }, [products])
 
