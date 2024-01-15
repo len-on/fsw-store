167a770de10b163d2efc58340e8ae623ccfef205
len-on <lenon.assalin@gmail.com>
2024-01-15 17:00:03 -0300

    feat: ensure order status is property formatted

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -11,6 +11,7 @@ import OrderProductItem from "./order-product-item";
 import { Separator } from "@/components/ui/separator";
 import { useMemo } from "react";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { getOrderStatus } from "../helpers/status";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
@@ -41,7 +42,7 @@ const OrderItem = ({ order }: OrderItemProps) => {
     }, 0);
   }, [order.orderProducts]);
 
-  const totalDiscounts = subtotal - total
+  const totalDiscounts = subtotal - total;
 
   return (
     <Card className="p-5">
@@ -50,7 +51,9 @@ const OrderItem = ({ order }: OrderItemProps) => {
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
               <p>Pedido com {order.orderProducts.length} produtos(s)</p>
-              <span className="text-sm opacity-60">Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}</span>
+              <span className="text-sm opacity-60">
+                Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}
+              </span>
             </div>
           </AccordionTrigger>
         </AccordionItem>
@@ -60,7 +63,7 @@ const OrderItem = ({ order }: OrderItemProps) => {
             <div className="flex place-items-center justify-between">
               <div className="font-bold">
                 <p>Status</p>
-                <p className="text-[#8162FF]">{order.status}</p>
+                <p className="text-[#8162FF]">{getOrderStatus(order.status)}</p>
               </div>
 
               <div>
